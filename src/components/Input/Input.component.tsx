62a53a5c5a0758afa3f26b98e5d8f80afa7d0dea
mikhaelesa <mikeul2.gt@gmail.com>
2023-01-26 00:03:29 +0700

    Update component docs, Add invalid input handling, Change Typography logics

--- src/components/Input/Input.component.tsx
+++ src/components/Input/Input.component.tsx
@@ -1,15 +1,65 @@
-import { InputHTMLAttributes } from "react";
+import { ChangeEvent, FormEvent, InputHTMLAttributes, useState } from "react";
+import { Typography } from "../Typography";
 
 export interface IInputProps extends InputHTMLAttributes<HTMLInputElement> {
 	error?: boolean;
+	errorMessage?: string;
 	label?: string;
 }
 
-export const Input = ({ error, label, className, id, ...props }: IInputProps) => {
+export const Input = ({
+	error = false,
+	errorMessage = "Invalid input",
+	label,
+	className,
+	id,
+	onInvalid,
+	onChange,
+	required = false,
+	...props
+}: IInputProps) => {
+	const [isInvalid, setIsInvalid] = useState(error);
+
+	const handleOnInvalid = (e: FormEvent<HTMLInputElement>) => {
+		e.preventDefault();
+		if (onInvalid) {
+			onInvalid(e);
+		}
+		setIsInvalid(true);
+	};
+
+	const handleOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+		if (onChange) {
+			onChange(e);
+		}
+		setIsInvalid(false);
+	};
+
 	return (
 		<div className={["Input", className].join(" ")}>
-			{label && <label htmlFor={id}>{label}</label>}
-			<input {...props} id={id} className={error ? "Input--Error" : ""} />
+			{label && (
+				<label htmlFor={id}>
+					{label}
+					{required && (
+						<Typography variant="body-2" component="span">
+							*
+						</Typography>
+					)}
+				</label>
+			)}
+			<input
+				{...props}
+				id={id}
+				required={required}
+				className={isInvalid ? "Input--Error" : ""}
+				onInvalid={handleOnInvalid}
+				onChange={handleOnChange}
+			/>
+			{isInvalid && errorMessage && (
+				<Typography className="Input--Error--Message" variant="body-3" component="span">
+					{errorMessage}
+				</Typography>
+			)}
 		</div>
 	);
 };
